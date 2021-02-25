215584f1d7b1630b1907497fca571250c7d28e4c
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-02-25 14:51:41 +0000

    Initial commit, adding githooks submodule

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# dataworks-repo-template-docker
+# dataworks-behavioural-framework
 
-## Description
+## Framework using behave to create behavioural scenarios for mainly quality assurance purposes
 
 This repo contains Makefile, and Dockerfile to fit the standard pattern.
 This repo is a base to create new Docker image repos, adding the githooks submodule, making the repo ready for use.
--- ecr.tf
+++ ecr.tf
@@ -1,16 +1,16 @@
-resource "aws_ecr_repository" "dataworks-repo-template-docker" {
-  name = "dataworks-repo-template-docker"
+resource "aws_ecr_repository" "dataworks-behavioural-framework" {
+  name = "dataworks-behavioural-framework"
   tags = merge(
     local.common_tags,
-    { DockerHub : "dwpdigital/dataworks-repo-template-docker" }
+    { DockerHub : "dwpdigital/dataworks-behavioural-framework" }
   )
 }
 
-resource "aws_ecr_repository_policy" "dataworks-repo-template-docker" {
-  repository = aws_ecr_repository.dataworks-repo-template-docker.name
+resource "aws_ecr_repository_policy" "dataworks-behavioural-framework" {
+  repository = aws_ecr_repository.dataworks-behavioural-framework.name
   policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
 }
 
 output "ecr_example_url" {
-  value = aws_ecr_repository.dataworks-repo-template-docker.repository_url
+  value = aws_ecr_repository.dataworks-behavioural-framework.repository_url
 }
