75ec9247b281f396f91e5a54f7c04c75ca069f3b
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-03-10 20:04:11 +0000

    Fixup: Format Python code with Black

--- src/features/steps/analytical-dataset-generation.py
+++ src/features/steps/analytical-dataset-generation.py
@@ -82,13 +82,15 @@ def step_(context, template_name):
 
 @then("start adg '{snapshot_type}' cluster and wait for the step '{step_name}'")
 def step_(context, snapshot_type, step_name):
-    context.adg_s3_prefix = os.path.join(context.mongo_snapshot_path, context.test_run_name)
+    context.adg_s3_prefix = os.path.join(
+        context.mongo_snapshot_path, context.test_run_name
+    )
     context.adg_export_date = datetime.now().strftime("%Y-%m-%d")
     payload = {
         CORRELATION_ID: context.test_run_name,
         S3_PREFIX: context.adg_s3_prefix,
         SNAPSHOT_TYPE: snapshot_type,
-        EXPORT_DATE: context.adg_export_date
+        EXPORT_DATE: context.adg_export_date,
     }
     payload_json = json.dumps(payload)
     cluster_response = invoke_lambda.invoke_adg_emr_launcher_lambda(payload_json)
@@ -176,18 +178,15 @@ def metadata_table_step_impl(context, snapshot_type):
     table_name = "data_pipeline_metadata"
 
     key_dict = {
-        "Correlation_Id": {"S": f"{context.test_run_name}"}, 
-        "DataProduct": {"S": f"{data_product}"}
+        "Correlation_Id": {"S": f"{context.test_run_name}"},
+        "DataProduct": {"S": f"{data_product}"},
     }
 
     console_printer.print_info(
         f"Getting DynamoDb data from item with key_dict of '{key_dict}' from table named '{table_name}'"
     )
 
-    response = aws_helper.get_item_from_dynamodb(
-        table_name, 
-        key_dict
-    )
+    response = aws_helper.get_item_from_dynamodb(table_name, key_dict)
 
     final_step = "executeUpdateAll"
     if snapshot_type.lower() == "full":
@@ -195,15 +194,30 @@ def metadata_table_step_impl(context, snapshot_type):
 
     console_printer.print_info(f"Data retrieved from dynamodb table : '{response}'")
 
-    assert "Item" in response, f"Could not find metadata table row with correlation id of '{context.test_run_name}' and data product  of '{data_product}'"
+    assert (
+        "Item" in response
+    ), f"Could not find metadata table row with correlation id of '{context.test_run_name}' and data product  of '{data_product}'"
 
     item = response["Item"]
     console_printer.print_info(f"Item retrieved from dynamodb table : '{item}'")
 
     assert item["TimeToExist"]["N"] is not None, f"Time to exist was not set"
-    assert item["Run_Id"]["N"] == "1", f"Run_Id was '{item['Run_Id']['N']}', expected '1'"
-    assert item["Date"]["S"] == context.adg_export_date, f"Date was '{item['Date']['S']}', expected '{context.adg_export_date}'"
-    assert (item["CurrentStep"]["S"] == "sns-notification" or item["CurrentStep"]['S'] == final_step), f"CurrentStep was '{item['CurrentStep']['S']}', expected 'sns-notification' or '{final_step}'"
-    assert item["Cluster_Id"]["S"] == context.adg_cluster_id, f"Cluster_Id was '{item['Cluster_Id']['S']}', expected '{context.adg_cluster_id}'"
-    assert item["S3_Prefix_Snapshots"]["S"] == context.adg_s3_prefix, f"S3_Prefix_Snapshots was '{item['S3_Prefix_Snapshots']['S']}', expected '{context.adg_s3_prefix}'"
-    assert item["Snapshot_Type"]["S"] == snapshot_type, f"Snapshot_Type was '{item['Snapshot_Type']['S']}', expected '{snapshot_type}'"
+    assert (
+        item["Run_Id"]["N"] == "1"
+    ), f"Run_Id was '{item['Run_Id']['N']}', expected '1'"
+    assert (
+        item["Date"]["S"] == context.adg_export_date
+    ), f"Date was '{item['Date']['S']}', expected '{context.adg_export_date}'"
+    assert (
+        item["CurrentStep"]["S"] == "sns-notification"
+        or item["CurrentStep"]["S"] == final_step
+    ), f"CurrentStep was '{item['CurrentStep']['S']}', expected 'sns-notification' or '{final_step}'"
+    assert (
+        item["Cluster_Id"]["S"] == context.adg_cluster_id
+    ), f"Cluster_Id was '{item['Cluster_Id']['S']}', expected '{context.adg_cluster_id}'"
+    assert (
+        item["S3_Prefix_Snapshots"]["S"] == context.adg_s3_prefix
+    ), f"S3_Prefix_Snapshots was '{item['S3_Prefix_Snapshots']['S']}', expected '{context.adg_s3_prefix}'"
+    assert (
+        item["Snapshot_Type"]["S"] == snapshot_type
+    ), f"Snapshot_Type was '{item['Snapshot_Type']['S']}', expected '{snapshot_type}'"
