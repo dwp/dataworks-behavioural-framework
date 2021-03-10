41be813443f1ac6afe69e6a0e3677581871f82fc
Connor Avery <55276424+connorIW@users.noreply.github.com>
2021-03-10 17:10:05 +0000

    Add ingestion-ecs-cluster start stop activities (#13)
    * Add ingestion-ecs-cluster start stop activities

--- src/features/fixtures/before_fixtures.py
+++ src/features/fixtures/before_fixtures.py
@@ -555,6 +555,25 @@ def k2hb_stop(context, timeout=30, **kwargs):
 
 
 @fixture
+def ingest_ecs_cluster_start(context, timeout=30, **kwargs):
+    console_printer.print_info("Executing 'ingestion_ecs_cluster_start' fixture")
+
+    context.last_scaled_asg = aws_helper.scale_asg_if_desired_count_is_not_already_set(
+        context.asg_prefix_ingestion_ecs_cluster,
+        int(context.asg_max_count_k2hb_main_london),
+    )
+
+
+@fixture
+def ingest_ecs_cluster_stop(context, timeout=30, **kwargs):
+    console_printer.print_info("Executing 'ingestion_ecs_cluster_stop' fixture")
+
+    context.last_scaled_asg = aws_helper.scale_asg_if_desired_count_is_not_already_set(
+        context.asg_prefix_ingestion_ecs_cluster, 0
+    )
+
+
+@fixture
 def ucfs_claimant_kafka_consumer_start(context, timeout=30, **kwargs):
     console_printer.print_info("Executing 'ucfs_claimant_kafka_consumer_start' fixture")
 
