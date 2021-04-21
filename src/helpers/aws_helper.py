4b07d80ec3b55fc2ceffc1d14a4d7635b04a989a
Levan Kirvalidze <levanik@live.com>
2021-04-21 15:46:52 +0100

    fix var name
    Signed-off-by: Levan Kirvalidze <levanik@live.com>

--- src/helpers/aws_helper.py
+++ src/helpers/aws_helper.py
@@ -203,7 +203,7 @@ def scan_dynamodb_with_filters(table_name, filters):
 
     # Dynamo returns integers with the word Decimal around them. The below sorts this out
     for item in response["Items"]:
-        clean_dict = ast.literal_eval((json.dumps(i, cls=DecimalEncoder)))
+        clean_dict = ast.literal_eval((json.dumps(item, cls=DecimalEncoder)))
     return clean_dict
 
 
