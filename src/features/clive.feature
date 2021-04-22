f0dfe67c882e61304e7a8dcabfc913f5b3ebae71
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-04-22 14:32:49 +0100

    add test for core contract
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- src/features/clive.feature
+++ src/features/clive.feature
@@ -2,7 +2,13 @@
 @test
 Feature: Clive tests, to run clive and validate its output
 
+  @fixture.terminate.adg.cluster
   Scenario: Using ADG output data, the Clive process creates Hive tables on this data, that is queryable and contains the data of the ADG output files.
     Given the results of the dynamodb table 'data_pipeline_metadata' for 'ADG-full'
-    When I start the CLIVE cluster and wait for the step 'run-clive'
+    Then start the CLIVE cluster and wait for the step 'run-clive' for a maximum of '240' minutes
+    And insert the 'hive-query' step onto the cluster
+    And wait a maximum of '60' minutes for the step to finish
+    Then the CLIVE result matches the expected results of 'core_contract_expected.csv'
+
+
 
--- src/features/steps/clive.py
+++ src/features/steps/clive.py
@@ -16,17 +16,6 @@ CLUSTER_ARN = "ClusterArn"
 COMPLETED_STATUS = "COMPLETED"
 
 
-# @given("the ADG uncompressed output '{input_json}' as an input data source on S3")
-# def step_(context, input_json):
-#     pass
-# expected_file_name = os.path.join(
-#     context.fixture_path_local, "pdm_data", "input_data", input_json
-# )
-# input_file = file_helper.get_contents_of_file(expected_file_name, False)
-# inputs_s3_key = os.path.join(context.pdm_test_input_s3_prefix, input_json)
-# aws_helper.put_object_in_s3(input_file, context.published_bucket, inputs_s3_key)
-
-
 @given("the results of the dynamodb table '{table_name}' for '{data_product}'")
 def step_(context, table_name, data_product):
     key_dict = {
@@ -40,15 +29,14 @@ def step_(context, table_name, data_product):
         f"Getting DynamoDb data with filters '{filters}' from table named '{table_name}'"
     )
 
+    # get items from the dynamo that are completed for adg-full
     response = aws_helper.scan_dynamodb_with_filters(table_name, filters)
 
-    console_printer.print_info(f"this is the type of it {type(response)}")
-
-    console_printer.print_info(f"this is the full response {response}")
-
+    # sort the items in the response by date
     response.sort(key=operator.itemgetter("Date"), reverse=True)
 
     latest_successfull_adg = {}
+    # get the first item from the sorted list. So the latest date
     for item in response:
         if "Date" in item and "S3_Prefix_Analytical_DataSet" in item:
             latest_successfull_adg = item
@@ -61,8 +49,11 @@ def step_(context, table_name, data_product):
     ]
 
 
-@when("I start the CLIVE cluster and wait for the step '{step_name}'")
-def step_(context, step_name):
+@then(
+    "start the CLIVE cluster and wait for the step '{step_name}' for a maximum of '{timeout_mins}' minutes"
+)
+def step_(context, step_name, timeout_mins):
+    timeout_secs = int(timeout_mins) * 60
     context.clive_export_date = datetime.now().strftime("%Y-%m-%d")
     emr_launcher_config = {
         "correlation_id": f"{context.test_run_name}",
@@ -85,7 +76,7 @@ def step_(context, step_name):
     console_printer.print_info(f"Step id for '{step_name}' : '{step_id}'")
     if step is not None:
         execution_state = aws_helper.poll_emr_cluster_step_status(
-            step_id, cluster_id, 2500
+            step_id, cluster_id, timeout_secs
         )
     if execution_state != COMPLETED_STATUS:
         raise AssertionError(
@@ -93,55 +84,64 @@ def step_(context, step_name):
         )
 
 
-# @when("insert the '{step_name}' step onto the cluster")
-# def step_impl(context, step_name):
-# context.clive_cluster_step_name = step_name
-# s3_path = f"{context.pdm_test_output_s3_prefix}/{context.test_run_name}"
-# file_name = f"{context.test_run_name}.csv"
-# pdm_hive_export_bash_command = (
-#     f"hive -e 'SELECT * FROM uc.youth_obligation_details_fact_v;' >> ~/{file_name} && "
-#     + f"aws s3 cp ~/{file_name} s3://{context.published_bucket}/{s3_path}/"
-#     + f" &>> /var/log/pdm/e2e.log"
-# )
-#
-# context.pdm_cluster_step_id = emr_step_generator.generate_bash_step(
-#     context.pdm_cluster_id,
-#     pdm_hive_export_bash_command,
-#     context.pdm_cluster_step_name,
-# )
-# context.pdm_results_s3_file = os.path.join(s3_path, file_name)
+@then("insert the '{step_name}' step onto the cluster")
+def step_impl(context, step_name):
+    context.clive_cluster_step_name = step_name
+    s3_path = f"{context.clive_test_input_s3_prefix}/{context.test_run_name}"
+    file_name = f"{context.test_run_name}.csv"
+    clive_hive_export_bash_command = (
+        f"hive -e 'SELECT * FROM uc_clive.contract;' >> ~/{file_name} && "
+        + f"aws s3 cp ~/{file_name} s3://{context.published_bucket}/{s3_path}/"
+        + f" &>> /var/log/clive/e2e.log"
+    )
 
-#
-# @when("wait a maximum of '{timeout_mins}' minutes for the step to finish")
-# def step_impl(context, timeout_mins):
-# timeout_secs = int(timeout_mins) * 60
-# execution_state = aws_helper.poll_emr_cluster_step_status(
-#     context.pdm_cluster_step_id, context.pdm_cluster_id, timeout_secs
-# )
-#
-# if execution_state != "COMPLETED":
-#     raise AssertionError(
-#         f"'{context.pdm_cluster_step_name}' step failed with final status of '{execution_state}'"
-#     )
+    context.clive_cluster_step_id = emr_step_generator.generate_bash_step(
+        context.clive_cluster_id,
+        clive_hive_export_bash_command,
+        context.clive_cluster_step_name,
+    )
+    context.clive_results_s3_file = os.path.join(s3_path, file_name)
 
 
-# @then("the CLIVE result matches the expected results of '{expected_result_file_name}'")
-# def step_(context, expected_result_file_name):
-# console_printer.print_info(f"S3 Request Location: {context.pdm_results_s3_file}")
-# actual = aws_helper.get_s3_object(
-#     None, context.published_bucket, context.pdm_results_s3_file
-# ).decode("ascii")
-# actual_comma_deliminated = actual.replace("\t", ",").strip()
-#
-# expected_file_name = os.path.join(
-#     context.fixture_path_local, "pdm_data", "expected", expected_result_file_name
-# )
-# expected = file_helper.get_contents_of_file(expected_file_name, False)
-# expected_comma_deliminated = expected.replace("\t", ",").strip()
-#
-# assert (
-#     expected_comma_deliminated == actual_comma_deliminated
-# ), f"Expected result of '{expected_comma_deliminated}', does not match '{actual_comma_deliminated}'"
+@then("wait a maximum of '{timeout_mins}' minutes for the step to finish")
+def step_impl(context, timeout_mins):
+    timeout_secs = int(timeout_mins) * 60
+    execution_state = aws_helper.poll_emr_cluster_step_status(
+        context.clive_cluster_step_id, context.clive_cluster_id, timeout_secs
+    )
+
+    if execution_state != "COMPLETED":
+        raise AssertionError(
+            f"'{context.clive_cluster_step_name}' step failed with final status of '{execution_state}'"
+        )
+
+
+@then("the CLIVE result matches the expected results of '{expected_result_file_name}'")
+def step_(context, expected_result_file_name):
+    console_printer.print_info(f"S3 Request Location: {context.clive_results_s3_file}")
+    actual = (
+        aws_helper.get_s3_object(
+            None, context.published_bucket, context.clive_results_s3_file
+        )
+        .decode("ascii")
+        .replace("\t", "")
+        .replace(" ", "")
+        .strip()
+    )
+
+    expected_file_name = os.path.join(
+        context.fixture_path_local, "clive", "expected", expected_result_file_name,
+    )
+    expected = (
+        file_helper.get_contents_of_file(expected_file_name, False)
+        .replace("\t", "")
+        .replace(" ", "")
+        .strip()
+    )
+
+    assert (
+        expected == actual
+    ), f"Expected result of '{expected}', does not match '{actual}'"
 
 
 @when("the CLIVE cluster tags have been created correctly")
