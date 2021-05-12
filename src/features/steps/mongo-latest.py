4e6b2b33b85d2b4b6cdcad52e42b8e4017e1da7d
steveburton4 <Steven.burton@hotmail.co.uk>
2021-05-12 17:48:07 +0100

    Fix s3 prefix (#52)

--- src/features/steps/mongo-latest.py
+++ src/features/steps/mongo-latest.py
@@ -167,8 +167,8 @@ def metadata_table_step_impl(context):
         item["Cluster_Id"]["S"] == context.mongo_latest_cluster_id
     ), f"Cluster_Id was '{item['Cluster_Id']['S']}', expected '{context.mongo_latest_cluster_id}'"
     assert (
-        item["S3_Prefix_Snapshots"]["S"] == context.mongo_latest_s3_prefix
-    ), f"S3_Prefix_Snapshots was '{item['S3_Prefix_Snapshots']['S']}', expected '{context.mongo_latest_s3_prefix}'"
+        item["S3_Prefix_Snapshots"]["S"] == "NOT_SET"
+    ), f"S3_Prefix_Snapshots was '{item['S3_Prefix_Snapshots']['S']}', expected 'NOT_SET'"
     assert (
         item["Snapshot_Type"]["S"] == snapshot_type
     ), f"Snapshot_Type was '{item['Snapshot_Type']['S']}', expected '{snapshot_type}'"
