e67c0ae655c7d6cb6c28c143ce5f48383966c064
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2021-06-07 07:44:22 +0100

    fixed

--- src/features/steps/analytical-dataset-generation.py
+++ src/features/steps/analytical-dataset-generation.py
@@ -42,7 +42,7 @@ ADG_FULL_TOPICS = [
     "db.core.claimantCommitment",
     "db.core.toDo",
     "db.accepted-data.personDetails",
-    "db.appointments.appointment",
+    "db.appointments.appointment"
 ]
 
 ADG_INCREMENTAL_TOPICS = [
@@ -56,14 +56,14 @@ ADG_INCREMENTAL_TOPICS = [
     "db.core.toDo",
     "db.accepted-data.personDetails",
     "db.appointments.appointment",
-    "data.businessAudit",
+    "data.businessAudit"
 ]
 
 ADG_DB_FULL_COLLECTION = {
     "agent-core": ["agent", "agentToDo", "team"],
     "core": ["statement", "contract", "claimant", "claimantCommitment", "toDo"],
     "accepted-data": ["personDetails"],
-    "appointments": ["appointment"],
+    "appointments": ["appointment"]
 }
 
 ADG_DB_INCREMENTAL_COLLECTION = {
@@ -71,7 +71,21 @@ ADG_DB_INCREMENTAL_COLLECTION = {
     "core": ["statement", "contract", "claimant", "claimantCommitment", "toDo"],
     "accepted-data": ["personDetails"],
     "appointments": ["appointment"],
-    "data": ["businessAudit"],
+    "data": ["businessAudit"]
+}
+
+ADG_DB_INCREMENTAL_COLLECTION_DATED_OUTPUT = {
+    "agent-core": ["agent", "agentToDo", "team"],
+    "core": ["statement", "contract", "claimant", "claimantCommitment", "toDo"],
+    "accepted-data": ["personDetails"],
+    "appointments": ["appointment"]
+}
+
+ADG_INCREMENTAL_TOPICS_DATED_OUTPUT = {
+    "agent-core": ["agent", "agentToDo", "team"],
+    "core": ["statement", "contract", "claimant", "claimantCommitment", "toDo"],
+    "accepted-data": ["personDetails"],
+    "appointments": ["appointment"]
 }
 
 
@@ -272,8 +286,8 @@ def step_verify_analytical_datasets(context, snapshot_type):
         ADG_DB_COLLECTION = ADG_DB_FULL_COLLECTION
         ADG_TOPICS = ADG_FULL_TOPICS
     else:
-        ADG_DB_COLLECTION = ADG_DB_INCREMENTAL_COLLECTION
-        ADG_TOPICS = ADG_INCREMENTAL_TOPICS
+        ADG_DB_COLLECTION = ADG_DB_INCREMENTAL_COLLECTION_DATED_OUTPUT
+        ADG_TOPICS = ADG_INCREMENTAL_TOPICS_DATED_OUTPUT
     assert len(keys) == len(ADG_TOPICS)
     for ADG_DB, collections in ADG_DB_COLLECTION.items():
         for collection in collections:
