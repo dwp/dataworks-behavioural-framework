d0353217e5750cb9c7f9930ebe5bf3df7896c8f0
Mark <mark.stevens@infinityworks.com>
2021-06-09 09:49:46 +0100

    matching pattern on all
    Signed-off-by: Mark <mark.stevens@infinityworks.com>

--- src/features/steps/data-egress-s3.py
+++ src/features/steps/data-egress-s3.py
@@ -104,7 +104,7 @@ def step_prepare_data_egress_test(context, template_name):
 def step_verify_data_egress_content(context):
     time.sleep(10)
     keys = aws_helper.get_s3_file_object_keys_matching_pattern(
-        context.published_bucket, S3_PREFIX_FOR_OUTPUT, OUTPUT_FILE_REGEX
+        context.published_bucket, S3_PREFIX_FOR_OUTPUT, ".*"
     )
     console_printer.print_info(f"Keys in data egress output location : {keys}")
     console_printer.print_info(f"Keys in data egress output location : {keys} 2")
