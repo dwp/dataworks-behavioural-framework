1960417939ce4a03d6c7183fe8e4720cf6957b87
Mark <mark.stevens@infinityworks.com>
2021-06-09 09:33:08 +0100

    chaged file content
    Signed-off-by: Mark <mark.stevens@infinityworks.com>

--- src/features/steps/data-egress-s3.py
+++ src/features/steps/data-egress-s3.py
@@ -34,7 +34,7 @@ def step_prepare_sft_test(context, template_name, file_location):
 
     console_printer.print_info(f"Executing commands on Ec2")
     ssm_client = aws_helper.get_client('ssm')
-    commands = ['echo "hello world"', 'sudo su', 'cd /var/lib/docker/volumes/data-egress/_data/test', 'echo "test" >> test1.txt']
+    commands = ['echo "hello world"', 'sudo su', 'cd /var/lib/docker/volumes/data-egress/_data/test', 'echo "e2e test" >> test1.txt']
     resp = ssm_client.send_command(
         DocumentName="AWS-RunShellScript", 
         Parameters={'commands': commands},
