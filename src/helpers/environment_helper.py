aaf4649103e6dfebb24a348161250d1c566491c9
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-09-27 18:52:19 +0000

    Fixup: Format Python code with Black

--- src/helpers/environment_helper.py
+++ src/helpers/environment_helper.py
@@ -397,7 +397,9 @@ def set_test_run_common_variables(context):
     ]
 
     context.cdl_run_script_s3_url = context.config.userdata.get("CDL_RUN_SCRIPT_S3_URL")
-    context.cdl_split_inputs_s3_url = context.config.userdata.get("CDL_SPLIT_INPUTS_S3_URL")
+    context.cdl_split_inputs_s3_url = context.config.userdata.get(
+        "CDL_SPLIT_INPUTS_S3_URL"
+    )
 
     context.hdl_run_script_s3_url = context.config.userdata.get("HDL_RUN_SCRIPT_S3_URL")
     context.create_hbase_tables_script_url = context.config.userdata.get(
