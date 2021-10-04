dea6eebdbf63e02406d863eb55fd2643ad4bb8e2
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-10-04 12:45:15 +0000

    Fixup: Format Python code with Black

--- src/helpers/dataworks_kafka_producer_helper.py
+++ src/helpers/dataworks_kafka_producer_helper.py
@@ -14,16 +14,12 @@ from helpers import aws_helper, console_printer
 
 
 def get_hsm_pub_key_id(p_hsm_key_id):
-    pub_key_id = aws_helper.get_ssm_parameter_value(
-        p_hsm_key_id
-    )
+    pub_key_id = aws_helper.get_ssm_parameter_value(p_hsm_key_id)
     return pub_key_id
 
 
 def get_hsm_pub_key(p_hsm_pub_key):
-    hsm_pub_key = aws_helper.get_ssm_parameter_value(
-        p_hsm_pub_key
-    )
+    hsm_pub_key = aws_helper.get_ssm_parameter_value(p_hsm_pub_key)
     return hsm_pub_key
 
 
@@ -40,7 +36,9 @@ def create_new_data_key(context):
     # Generate a random data key
     data_key = get_random_bytes(16)
     # Get the public key from SSM
-    hsm_pub_key = base64.b64decode(get_hsm_pub_key(context.dataworks_streams_kafka_producer_hsm_pub_key))
+    hsm_pub_key = base64.b64decode(
+        get_hsm_pub_key(context.dataworks_streams_kafka_producer_hsm_pub_key)
+    )
     # Create an encrypted data key using public key and data key
     cipher_rsa = create_cipher(hsm_pub_key)
     enc_data_key = create_encrypted_data_key(cipher_rsa, data_key)
@@ -62,7 +60,9 @@ def encrypt_data_aes_ctr(context, data_key, plaintext_string, iv=None):
     aes = AES.new(data_key, AES.MODE_CTR, counter=counter)
     ciphertext = aes.encrypt(plaintext_string.encode("utf8"))
     ciphertext = base64.b64encode(ciphertext)
-    encryption_key_id = get_hsm_pub_key_id(context.dataworks_streams_kafka_producer_hsm_key_id)
+    encryption_key_id = get_hsm_pub_key_id(
+        context.dataworks_streams_kafka_producer_hsm_key_id
+    )
     return ciphertext.decode("ascii"), iv.decode("ascii"), encryption_key_id
 
 
