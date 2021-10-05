39ea4df3a31531b96442caf8c580cab4cd71ef07
vipinchadha <84083585+vipinchadha@users.noreply.github.com>
2021-10-05 17:10:06 +0100

    BYUC-939 - Enable e2e tests on QA (#154)
    * BYUC-939 - Enable e2e tests in QA
    
    * BYUC-939 - Enable e2e tests in QA
    
    * Fixup: Format Python code with Black
    
    Co-authored-by: DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>

--- src/helpers/dataworks_kafka_producer_helper.py
+++ src/helpers/dataworks_kafka_producer_helper.py
@@ -1,52 +1,14 @@
 import base64
-import os
 import pathlib
 
 import binascii
 from Crypto import Random
-from Crypto.Cipher import AES, PKCS1_OAEP
-from Crypto.Hash import SHA256
-from Crypto.PublicKey import RSA
-from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
 from Crypto.Util import Counter
-
-from helpers import aws_helper, console_printer
-
-
-def get_hsm_pub_key_id(p_hsm_key_id):
-    pub_key_id = aws_helper.get_ssm_parameter_value(p_hsm_key_id)
-    return pub_key_id
-
-
-def get_hsm_pub_key(p_hsm_pub_key):
-    hsm_pub_key = aws_helper.get_ssm_parameter_value(p_hsm_pub_key)
-    return hsm_pub_key
-
-
-def create_cipher(hsm_pub_key):
-    hsm_key = RSA.import_key(hsm_pub_key)
-    return PKCS1_OAEP.new(key=hsm_key, hashAlgo=SHA256)
-
-
-def create_encrypted_data_key(cipher_rsa, data_key):
-    return cipher_rsa.encrypt(data_key)
-
-
-def create_new_data_key(context):
-    # Generate a random data key
-    data_key = get_random_bytes(16)
-    # Get the public key from SSM
-    hsm_pub_key = base64.b64decode(
-        get_hsm_pub_key(context.dataworks_streams_kafka_producer_hsm_pub_key)
-    )
-    # Create an encrypted data key using public key and data key
-    cipher_rsa = create_cipher(hsm_pub_key)
-    enc_data_key = create_encrypted_data_key(cipher_rsa, data_key)
-    return data_key, hsm_pub_key, base64.b64encode(enc_data_key)
 
 
 # Encrypt the data using AES CTR algorithm
-def encrypt_data_aes_ctr(context, data_key, plaintext_string, iv=None):
+def encrypt_data_aes_ctr(data_key, plaintext_string, iv=None):
     if iv is None:
         initialisation_vector = Random.new().read(AES.block_size)
         iv_int = int(binascii.hexlify(initialisation_vector), 16)
@@ -60,10 +22,8 @@ def encrypt_data_aes_ctr(context, data_key, plaintext_string, iv=None):
     aes = AES.new(data_key, AES.MODE_CTR, counter=counter)
     ciphertext = aes.encrypt(plaintext_string.encode("utf8"))
     ciphertext = base64.b64encode(ciphertext)
-    encryption_key_id = get_hsm_pub_key_id(
-        context.dataworks_streams_kafka_producer_hsm_key_id
-    )
-    return ciphertext.decode("ascii"), iv.decode("ascii"), encryption_key_id
+
+    return ciphertext.decode("ascii"), iv.decode("ascii")
 
 
 def read_test_data(file_name):
