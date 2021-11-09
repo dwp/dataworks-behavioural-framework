a740a90f8f77ae5d54f4b78f335abed162cf96b9
PhanindraVenkata <phanindra.venkata@dwp.gsi.gov.uk>
2021-11-09 10:56:18 +0000

    DW-7708-Add changing the path of  uc_mongo_latest from published to processed

--- src/features/environment.py
+++ src/features/environment.py
@@ -147,7 +147,7 @@ def before_tag(context, tag):
     if tag == "fixture.setup.rbac_uc_ris_unredacted__all":
         use_fixture(before_fixtures.setup_user, context, "rbac_uc_ris_unredacted__all")
     if tag == "fixture.setup.rbac_uc__auditlog_redacted_and_sc_and_role":
-        use_fixture(rbac_uc_mongo_latest__uc_mongo_latest__full_access_and_role
+        use_fixture(
             before_fixtures.setup_user_and_role,
             context,
             "rbac_uc__auditlog_redacted",
