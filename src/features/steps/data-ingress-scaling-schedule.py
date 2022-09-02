c08a82d7dd8317fc94b113eed2bc270d26aaf0c5
camillascf <camilla.scuffi@credera.co.uk>
2022-09-02 14:47:28 +0100

    merge previous change

--- src/features/steps/data-ingress-scaling-schedule.py
+++ src/features/steps/data-ingress-scaling-schedule.py
@@ -1,52 +1,35 @@
-from behave import given, when, then
+from behave import given, then
 import time
 from helpers import (
     aws_helper,
-    data_ingress_helper,
-    console_printer
+    console_printer,
+    data_ingress_helper
 )
 
-ASG = 'data-ingress-ag'
-CLUSTER = 'data-ingress'
-FILENAME = 'TestBasicCompanyData'
-S3_PREFIX = 'data-ingress/companies'
 
-
-@given("the autoscaling schedules replicas that are set to scale up after 'time_scale_up' min and scale down after 'time_scale_down' min")
+@given("the autoscaling schedules replicas that are set to scale up after '{time_scale_up}' min and scale down after '{time_scale_down}' min")
 def step_impl(context, time_scale_up, time_scale_down):
-    context.time_scale_up = time_scale_up
-    context.time_scale_down = time_scale_down
-    context.max_wait = 4*60
+    try:
+        context.time_scale_up = int(time_scale_up)
+        context.time_scale_down = int(time_scale_down)
+    except Exception as ex:
+        console_printer.print_error_text(ex)
 
 
 @then("wait for the instance to scale up within the expected time")
 def step_impl(context):
-    time.sleep(context.time_scale_up*60)
-    t0 = time.time()
-    t1 = t0 + context.max_wait
-    scaled = False
-    while time.time() < t1 and scaled == False:
-        if aws_helper.instance_count_by_tag("aws:autoscaling:groupName", ASG) == 1:
-            scaled = True
-            console_printer.print_info("instance count reached desired size within the time frame given")
-        time.sleep(5)
-    if scaled == False:
-        console_printer.print_error_text("instance count did not reached desired size within the time frame given")
-        raise AssertionError("step scale up failed")
+
+    w = context.time_scale_up*60
+    console_printer.print_info(f"waiting {w} seconds")
+    time.sleep(w)
+    data_ingress_helper.check_instance_count(desired_count=1)
+    console_printer.print_info("scaling successful")
 
 
 @then("wait for the instance to scale down within the expected time")
 def step_impl(context):
-    time.sleep(context.time_scale_down*60)
-    t0 = time.time()
-    t1 = t0 + context.max_wait
-    scaled = False
-    while time.time() < t1 and scaled == False:
-        if aws_helper.instance_count_by_tag("aws:autoscaling:groupName", ASG) == 0:
-            scaled = True
-            console_printer.print_info("instance count reached desired size within the time frame given")
-        time.sleep(5)
-    if scaled == False:
-        console_printer.print_error_text("instance count did not reached desired size within the time frame given")
-        raise AssertionError("step scale down failed")
-
+    w = context.time_scale_down*60 - context.time_scale_up*60
+    console_printer.print_info(f"waiting {w} seconds")
+    time.sleep(w)
+    data_ingress_helper.check_instance_count(desired_count=0)
+    console_printer.print_info("scaling successful")
--- src/helpers/data_ingress_helper.py
+++ src/helpers/data_ingress_helper.py
@@ -1,5 +1,10 @@
-from helpers import aws_helper
 import datetime
+import time
+from helpers import (
+    aws_helper,
+    console_printer,
+    data_ingress_helper
+)
 
 
 def restart_service(service, cluster):
@@ -19,3 +24,20 @@ def set_asg_instance_count(asg_name, min, max, desired):
         DesiredCapacity=desired,
         TimeZone='Europe/London'
     )
+
+
+def check_instance_count(desired_count, max_wait=120):
+    t0 = time.time()
+    t1 = t0 + max_wait
+    ic = "unknown"
+    while time.time() < t1:
+        ic = aws_helper.instance_count_by_tag("aws:autoscaling:groupName", "data-ingress-ag")
+        console_printer.print_info(f"instance count: {ic}")
+        s = t1 - time.time()
+        console_printer.print_info(f"seconds before timeout: {round(s)}")
+        if ic == desired_count:
+            console_printer.print_info(f"instances scaled up to {ic} within the time frame given")
+            break
+        time.sleep(10)
+    if ic != desired_count:
+        raise AssertionError(f"instance count: {ic} did not reach desired size: {desired_count} within the time frame given")
