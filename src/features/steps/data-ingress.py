4c87f73cca5103e8fb01c370915bd591afe5b680
Camilla Scuffi <camilla.scuffi@credera.co.uk>
2022-09-22 10:39:20 +0100

    correct wait time

--- src/features/steps/data-ingress.py
+++ src/features/steps/data-ingress.py
@@ -17,7 +17,8 @@ PASS_FILE_KEY = "e2e/eicar_test/not_passed.txt"
 TIMEOUT = 300
 
 
-@given("the autoscaling schedules replicas that are set to scale up after '{time_scale_up}' min and scale down after '{time_scale_down}' min")
+@given("the autoscaling schedules replicas that are set to scale up after '{time_scale_up}' min"
+       "and scale down after '{time_scale_down}' min")
 def step_impl(context, time_scale_up, time_scale_down):
     try:
         context.time_scale_up = int(time_scale_up)
@@ -74,7 +75,7 @@ def step_impl(context):
     filename = FILENAME+td+'.csv'
     console_printer.print_info(f"checking if file {filename} is present on s3 bucket")
     start = time.time()
-    while not aws_helper.check_if_s3_object_exists(context.data_ingress_stage_bucket, os.path.join(S3_PREFIX, filename)):
+    while not aws_helper.check_if_s3_object_exists(context.data_ingress_stage_bucket,os.path.join(S3_PREFIX, filename)):
         if time.time()-start < TIMEOUT:
             time.sleep(5)
         else:
@@ -84,7 +85,7 @@ def step_impl(context):
 @then("wait for the instance to scale down within the expected time")
 def step_impl(context):
     time_now = time.time()
-    w = context.time_scale_down - (time_now - context.time_start)
+    w = (context.time_scale_down * 60) - (time_now - context.time_start)
     if w > 0:
         console_printer.print_info(f"waiting {w} seconds")
         time.sleep(w)
