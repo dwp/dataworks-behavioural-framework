29f527d60d9c402e8da32d7cda7fe68fe4acdaad
Janis <40893585+jcakstins@users.noreply.github.com>
2022-11-21 14:42:38 +0000

    Reconfigured to the new sqs and output bucket (#196)
    Co-authored-by: Janis Cakstins <janis.cakstins@engineering.digital.dwp.gov.uk>

--- src/features/steps/dataworks-kafka-producer-app.py
+++ src/features/steps/dataworks-kafka-producer-app.py
@@ -3,7 +3,7 @@ import os
 
 import time
 from behave import given, when, then
-from helpers import aws_helper, dataworks_kafka_producer_helper
+from helpers import aws_helper, dataworks_kafka_producer_helper, console_printer
 
 
 @given("the e2e kafka producer app is running")
@@ -20,27 +20,14 @@ def step_impl(context):
 def step_impl(context, file_name):
     # Read the data file
     plaintext_string = dataworks_kafka_producer_helper.read_test_data(file_name)
-    data_key = base64.b64decode(context.encryption_plaintext_key)
-    # Encrypt the data
-    (encrypted_data, iv,) = dataworks_kafka_producer_helper.encrypt_data_aes_ctr(
-        plaintext_string=plaintext_string, data_key=data_key
-    )
-
-    # Set metadata
-    metadata = {
-        "iv": iv,
-        "ciphertext": context.encryption_encrypted_key,
-        "datakeyencryptionkeyid": context.encryption_master_key_id,
-    }
 
     # s3 object key
     object_key = f"{context.dataworks_model_output_s3_prefix}/{file_name}"
-
-    # Upload the data to s3
-    aws_helper.put_object_in_s3_with_metadata(
-        body=encrypted_data,
+    console_printer.print_info(f"Uploading file to S3: {object_key}")
+    
+    aws_helper.put_object_in_s3(
+        body=plaintext_string,
         s3_bucket=context.dataworks_model_output_s3_bucket,
-        metadata=metadata,
         s3_key=object_key,
     )
 
@@ -56,5 +43,5 @@ def step_impl(context, expected_lag):
     linux_command = "sh /home/ec2-user/kafka/utils/run_get_topic_last_offset.sh"
     response = aws_helper.execute_linux_command(instance_id, linux_command)
     actual_lag = response["StandardOutputContent"].rstrip()
-    msg = f"Mismatch in actual ({actual_lag}) and expected lag ({expected_lag})."
+    msg = f"Mismatch in actual ({actual_lag}) and expected lag ({expected_lag}).\nResponse:\t{response}\nStandardOutputContent:\t{response['StandardOutputContent']}"
     assert actual_lag == expected_lag, msg
