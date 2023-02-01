2592b4911c8a8b02c6d944049f52fc91fc4841f7
Camilla Scuffi <camilla.scuffi@credera.co.uk>
2023-02-01 14:22:04 +0000

    update filename

--- src/features/steps/dataworks-aws-ch.py
+++ src/features/steps/dataworks-aws-ch.py
@@ -67,9 +67,9 @@ def step_impl(context):
     )
     cols = ast.literal_eval(context.args_ch["args"]["cols"])
     console_printer.print_info(f"generating file1.csv ")
-    ch_helper.generate_csv_file("file1.csv", 0.02, cols)
+    ch_helper.generate_csv_file("file1.csv", 0.01, cols)
     console_printer.print_info(f"generating file2.csv")
-    ch_helper.generate_csv_file("file2.csv", 0.04, cols)
+    ch_helper.generate_csv_file("file2.csv", 0.02, cols)
     file = open("file2.csv")
     reader = csv.reader(file)
     lines = len(list(reader))
@@ -94,13 +94,13 @@ def step_impl(context):
     ch_helper.s3_upload(context, "file1.csv", E2E_S3_PREFIX, context.filenames[0])
     ch_helper.s3_upload(context, "file2.csv", E2E_S3_PREFIX, context.filenames[1])
     context.filename_not_to_process = context.filenames[0]
-    context.filename_expected = context.filenames[1]
+    context.filename_expected = context.filenames[-1]
 
 
 @when("Set the dynamo db bookmark on the first filename generated")
 def step_impl(context):
     ch_helper.add_latest_file(
-        context, os.path.basename(context.filenames[0].replace(".csv", ".zip")))
+        context, context.filenames[0])
 
 
 @then("Etl step in e2e mode completes")
@@ -206,8 +206,8 @@ def step_impl(context):
     console_printer.print_info(f"generating files with one extra column")
     cols = ast.literal_eval(context.args_ch["args"]["cols"])
     cols.update({"extra_column": "string"})
-    ch_helper.generate_csv_file(context.filenames[0], 0.02, cols)
-    ch_helper.generate_csv_file(context.filenames[1], 0.04, cols)
+    ch_helper.generate_csv_file(context.filenames[0], 0.01, cols)
+    ch_helper.generate_csv_file(context.filenames[1], 0.02, cols)
 
 
 @when("Generate files having incorrect headers for negative testing")
@@ -217,8 +217,8 @@ def step_impl(context):
     cols.pop(list(cols.keys())[-1])
     cols.pop(list(cols.keys())[-1])
     cols.update({"incorrect_colname_1": "string", "incorrect_colname_2": "string"})
-    ch_helper.generate_csv_file(context.filenames[0], 0.02, cols)
-    ch_helper.generate_csv_file(context.filenames[1], 0.04, cols)
+    ch_helper.generate_csv_file(context.filenames[0], 0.01, cols)
+    ch_helper.generate_csv_file(context.filenames[1], 0.02, cols)
 
 
 @when("Generate files having a row with string values instead of int")
@@ -227,8 +227,8 @@ def step_impl(context):
         f"generating files with one missing field for negative testing"
     )
     cols = ast.literal_eval(context.args_ch["args"]["cols"])
-    ch_helper.generate_csv_file_string_instead_of_int(context.filenames[0], 0.02, cols)
-    ch_helper.generate_csv_file_string_instead_of_int(context.filenames[1], 0.04, cols)
+    ch_helper.generate_csv_file_string_instead_of_int(context.filenames[0], 0.01, cols)
+    ch_helper.generate_csv_file_string_instead_of_int(context.filenames[1], 0.02, cols)
 
 
 @when("Generate files having a row with one missing field for negative testing")
@@ -237,5 +237,5 @@ def step_impl(context):
         f"generating files with one missing field for negative testing"
     )
     cols = ast.literal_eval(context.args_ch["args"]["cols"])
-    ch_helper.generate_csv_file_row_with_missing_field(context.filenames[0], 0.02, cols)
-    ch_helper.generate_csv_file_row_with_missing_field(context.filenames[1], 0.04, cols)
+    ch_helper.generate_csv_file_row_with_missing_field(context.filenames[0], 0.01, cols)
+    ch_helper.generate_csv_file_row_with_missing_field(context.filenames[1], 0.02, cols)
