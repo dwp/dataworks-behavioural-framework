eee899042ab95ce7a007f4f31ad03de07b4c1162
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2023-10-20 12:32:01 +0000

    Fixup: Format Python code with Black

--- src/features/steps/data-egress-txr.py
+++ src/features/steps/data-egress-txr.py
@@ -9,16 +9,17 @@ from helpers import (
 
 S3_PREFIX_FOR_SFT_OUTPUT = "e2e/data-egress/txr/"
 
+
 @given("a set of collections")
 def step_prepare_sft_test(context):
     context.txr_test_collections = []
 
     for row in context.table:
         context.txr_test_collection.append(
-            { 
+            {
                 "name": row["name"],
                 "file": f"{row['name']}-123-123-123456.12345678.txt.gz.enc",
-                "destination": row["destination"]    
+                "destination": row["destination"],
             }
         )
 
@@ -35,7 +36,7 @@ def step_submit_files_to_sft(context, data_directory):
         commands = [
             "sudo su",
             f"cd /var/lib/docker/volumes/data-egress/_data/{data_directory}",
-            f"echo 'test content' >> {collection['file']}"
+            f"echo 'test content' >> {collection['file']}",
         ]
         aws_helper.execute_commands_on_ec2_by_tags_and_wait(
             commands, ["dataworks-aws-data-egress"], 30
@@ -48,14 +49,15 @@ def step_verify_stf_content(context):
 
     for collection in context.txt_test_collections:
         console_printer.print_info(f"asserting against : {collection['name']}")
-        output_file_content = aws_helper.get_s3_object(
-            bucket=context.data_ingress_stage_bucket, 
-            key=f"{S3_PREFIX_FOR_SFT_OUTPUT}{collection['destination']}/{collection['file']}",
-            s3_client=None
-        ).decode().strip()
+        output_file_content = (
+            aws_helper.get_s3_object(
+                bucket=context.data_ingress_stage_bucket,
+                key=f"{S3_PREFIX_FOR_SFT_OUTPUT}{collection['destination']}/{collection['file']}",
+                s3_client=None,
+            )
+            .decode()
+            .strip()
+        )
 
         console_printer.print_info(f"sft file content is : {output_file_content}")
-        assert (
-            output_file_content
-            == "test content"
-        )
+        assert output_file_content == "test content"
