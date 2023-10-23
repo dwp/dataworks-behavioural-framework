baf227bb0c93f3e70c66d72f30e14ea5c71b09bb
rorybryett <rory.bryett@dwp.gov.uk>
2023-10-23 10:25:04 +0100

    chore: check for the absense of files where we dont expect them in S3
    Signed-off-by: rorybryett <rory.bryett@dwp.gov.uk>

--- src/features/data-egress-txr.feature
+++ src/features/data-egress-txr.feature
@@ -13,9 +13,9 @@ Feature: Data egress transfer txr data to S3 end to end test
         |   db.crypto.encryptedData-unencrypted         |   ris,cre         |
         |   db.organisation.organisation                |   ris,cre         |
         |   db.agent-core.agent                         |   ris,cre         |
-        |   db.core.todo                                |   ris             |
-        |   db.accepted-data.other                      |   ris             |
-        |   db.crypto.encryptedData                     |   ris             |
-        |   db.calculation.calculationParts             |   ris             |
+        |   db.core.todo                                |   ris,!cre        |
+        |   db.accepted-data.other                      |   ris,!cre        |
+        |   db.crypto.encryptedData                     |   ris,!cre        |
+        |   db.calculation.calculationParts             |   ris,!cre        |
     When we submit them to the 'RIS' data directory on the SFT service
     Then we verify the collection files are correctly distributed in S3
--- src/features/steps/data-egress-txr.py
+++ src/features/steps/data-egress-txr.py
@@ -1,4 +1,5 @@
 from behave import given, when, then
+from botocore.exceptions import ClientError
 import time
 from helpers import (
     aws_helper,
@@ -40,9 +41,7 @@ def step_submit_files_to_sft(context, data_directory):
         )
         commands.append(f"echo 'test content' >> {collection['file']}")
 
-    console_printer.print_info(
-        "Executing the following commands:'{}'".format("\n".join(commands))
-    )
+    console_printer.print_info("Executing the following commands:'{}'".format('\n'.join(commands)))
     aws_helper.execute_commands_on_ec2_by_tags_and_wait(
         commands, ["dataworks-aws-data-egress"], 5
     )
@@ -59,29 +58,26 @@ def step_verify_stf_content(context):
     for collection in context.txr_test_collections:
         destinations = collection["destination"].split(",")
         for dest in destinations:
-            console_printer.print_info(
-                f"Checking for collection '{collection['name']}' in destination '{dest}' on S3"
-            )
+            console_printer.print_info(f"Checking for collection '{collection['name']}' in destination '{dest}' on S3")
             try:
-                output_file_content = (
-                    aws_helper.get_s3_object(
-                        bucket=context.data_ingress_stage_bucket,
-                        key=f"{S3_PREFIX_FOR_SFT_OUTPUT}{dest}/{collection['file']}",
-                        s3_client=None,
-                    )
-                    .decode()
-                    .strip()
-                )
-                console_printer.print_info(
-                    f"SFT file content is: '{output_file_content}'"
-                )
+                output_file_content = aws_helper.get_s3_object(
+                    bucket=context.data_ingress_stage_bucket,
+                    key=f"{S3_PREFIX_FOR_SFT_OUTPUT}{dest}/{collection['file']}",
+                    s3_client=None
+                ).decode().strip()
+                console_printer.print_info(f"SFT file content is: '{output_file_content}'")
                 if output_file_content != "test content":
                     collections_that_do_not_reconcile += 1
+            except ClientError as e:
+                error_code = e.response["Error"]["Code"]
+                if error_code == "NoSuchKey":
+                    if dest[0] != "!":
+                        console_printer.print_error_text(f"Expecting collection '{collection['file']}' in destination '{dest}' on S3: {e}")
+                        missing_collections_in_destination += 1
+                else:
+                    raise e
             except Exception as e:
-                console_printer.print_error_text(
-                    f"Expecting collection '{collection['file']}' in destination '{dest}' on S3: {e}"
-                )
-                missing_collections_in_destination += 1
+                console_printer.print_error_text(f"Could not read object in S3 for collection '{collection['file']}' in destination '{dest}' on S3: {e}")
 
-    assert collections_that_do_not_reconcile == 0
-    assert missing_collections_in_destination == 0
+    assert ( collections_that_do_not_reconcile == 0 )
+    assert ( missing_collections_in_destination == 0 )
